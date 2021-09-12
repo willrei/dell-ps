cfab96d8bd119c5061ee272ed066be3d1e45781f
Willian Reichert <willian.reichert@inf.ufrgs.br>
2021-09-12 15:57:49 -0300

    basic layout

--- database.py
+++ database.py
@@ -2,53 +2,72 @@ import sqlite3 as sq
 import unicodedata
 
 FILE = 'gerint.csv'
-db = sq.connect(':memory:')
 
-def start():
-    db.execute('''
-        CREATE TABLE IF NOT EXISTS internacoes
-        (
-            data_extracao TEXT,
-            id_usuario TEXT,
-            situacao TEXT,
-            central_regulacao_origem TEXT,
-            data_solicitacao TEXT,
-            sexo TEXT,
-            idade REAL,
-            municipio_residencia TEXT,
-            solicitante TEXT,
-            municipio_solicitante TEXT,
-            codigo_cid TEXT, carater TEXT,
-            tipo_internacao TEXT,
-            tipo_leito TEXT,
-            data_autorizacao TEXT,
-            data_internacao TEXT,
-            data_alta TEXT,
-            executante TEXT,
-            horas_na_fila INTEGER
-        )'''
-    )
+class Database:
+    def __init__(self):
+        self.db = sq.connect(':memory:')
+        self.fields = []
+        self.field_count = 0
 
-    def normalize(word):
-        return unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('ascii')
+    def start(self):
+        self.db.execute('''
+            CREATE TABLE IF NOT EXISTS internacoes
+            (
+                data_extracao TEXT,
+                id_usuario TEXT,
+                situacao TEXT,
+                central_regulacao_origem TEXT,
+                data_solicitacao TEXT,
+                sexo TEXT,
+                idade REAL,
+                municipio_residencia TEXT,
+                solicitante TEXT,
+                municipio_solicitante TEXT,
+                codigo_cid TEXT, carater TEXT,
+                tipo_internacao TEXT,
+                tipo_leito TEXT,
+                data_autorizacao TEXT,
+                data_internacao TEXT,
+                data_alta TEXT,
+                executante TEXT,
+                horas_na_fila INTEGER
+            )'''
+        )
 
-    with open(FILE, 'r', encoding='utf-8') as file:
-        fields = file.readline().rstrip().split(';')
-        field_count = len(fields)
-        idade_index = fields.index('idade')
-        horas_index = fields.index('horas_na_fila')
+        def normalize(word):
+            return unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('ascii')
 
-        for line in file:
-            data = line.rstrip().split(';')
-            data = [normalize(item) for item in data]
-            data[idade_index] = float(data[idade_index]) if data[idade_index] != '' else 0.0
-            data[horas_index] = int(data[horas_index])
-            db.execute(f'INSERT INTO internacoes VALUES ({"?, " * (field_count - 1)}?)', data)
+        with open(FILE, 'r', encoding='utf-8') as file:
+            self.fields = file.readline().rstrip().split(';')
+            print(self.fields)
+            self.field_count = len(self.fields)
+            idade_index = self.fields.index('idade')
+            horas_index = self.fields.index('horas_na_fila')
 
-        db.commit()
+            for line in file:
+                data = line.rstrip().split(';')
+                data = [normalize(item) for item in data]
+                data[idade_index] = float(data[idade_index]) if data[idade_index] != '' else 0.0
+                data[horas_index] = int(data[horas_index])
+                self.db.execute(f'INSERT INTO internacoes VALUES ({"?, " * (self.field_count - 1)}?)', data)
 
-def get():
-    return db.execute('SELECT * FROM internacoes').fetchall()
+            self.db.commit()
 
-def close():
-    db.close()
+    def get(self):
+        return self.db.execute('SELECT * FROM internacoes').fetchall()
+
+    def close(self):
+        self.db.close()
+
+    def get_municipio(self, municipio: str):
+        count = self.db.execute(f'SELECT COUNT(*) FROM internacoes WHERE municipio_residencia = "{municipio}"').fetchone()[0]
+        print(f'Número de pacientes em {municipio.title()}: {count}')
+
+        avg_age = [0] * 2
+        for index, sex in enumerate(['MASCULINO', 'FEMININO']):
+            avg_age[index] = self.db.execute(f'SELECT AVG(idade) FROM internacoes WHERE municipio_residencia = "{municipio}" AND sexo = "{sex}"').fetchone()[0]
+        print(f'Média de idade de pacientes masculinos em {municipio.title()}: {avg_age[0]}')
+        print(f'Média de idade de pacientes femininos em {municipio.title()}: {avg_age[1]}')
+
+        print(f'Media de idade dos pacientes em {municipio.title()}: {sum(avg_age) / 2}')
+        return self.db.execute(f'SELECT * FROM internacoes WHERE municipio_residencia = "{municipio}"').fetchall()
--- gui.py
+++ gui.py
@@ -1,24 +1,92 @@
 import dearpygui.dearpygui as dpg
-import database as db
+import database
 
 vp = dpg.create_viewport(title='Internacoes')
 dpg.set_viewport_small_icon('python.ico')
 
-with dpg.window(label='aaa', menubar=False) as window:
-    dpg.add_button(label='1')
-    dpg.add_button(label='2')
-    dpg.add_button(label='3')
-    dpg.add_button(label='4')
-    dpg.add_button(label='5')
+with dpg.font_registry():
+    dpg.add_font('Roboto.ttf', 15, default_font=True)
+
+db = database.Database()
+db.start()
+
+with dpg.window(menubar=False) as window:
+
+    with dpg.tab_bar():
+        with dpg.tab(label='1: Media de Idade'):
+            with dpg.table(header_row=True, row_background=True,
+                        borders_innerH=True, borders_outerH=True,
+                        borders_innerV=True, borders_outerV=True, resizable=True,
+                        scrollX=True, policy=dpg.mvTable_SizingFixedFit):
+                for field in db.fields:
+                    dpg.add_table_column(label=field.replace('_', ' ').title())
+                data = db.get_municipio('VIAMAO')
+                for row in range(len(data)):
+                    for col in range(db.field_count):
+                        dpg.add_text(f'{data[row][col]}')
+                        if not (row == len(data) - 1 and col == db.field_count - 1):
+                            dpg.add_table_next_column()
+
+        with dpg.tab(label='2: Internacoes por Ano'):
+            with dpg.table(header_row=True, row_background=True,
+                        borders_innerH=True, borders_outerH=True,
+                        borders_innerV=True, borders_outerV=True, resizable=True,
+                        scrollX=True, policy=dpg.mvTable_SizingFixedFit):
+                for field in db.fields:
+                    dpg.add_table_column(label=field.replace('_', ' ').title())
+                data = db.get_municipio('XANGRI-LA')
+                for row in range(len(data)):
+                    for col in range(db.field_count):
+                        dpg.add_text(f'{data[row][col]}')
+                        if not (row == len(data) - 1 and col == db.field_count - 1):
+                            dpg.add_table_next_column()
+
+        with dpg.tab(label='3: Hospitais'):
+            with dpg.table(header_row=True, row_background=True,
+                        borders_innerH=True, borders_outerH=True,
+                        borders_innerV=True, borders_outerV=True, resizable=True,
+                        scrollX=True, policy=dpg.mvTable_SizingFixedFit):
+                for field in db.fields:
+                    dpg.add_table_column(label=field.replace('_', ' ').title())
+                data = db.get_municipio('CACHOEIRINHA')
+                for row in range(len(data)):
+                    for col in range(db.field_count):
+                        dpg.add_text(f'{data[row][col]}')
+                        if not (row == len(data) - 1 and col == db.field_count - 1):
+                            dpg.add_table_next_column()
+
+        with dpg.tab(label='4: Tempo de Internacao'):
+            with dpg.table(header_row=True, row_background=True,
+                        borders_innerH=True, borders_outerH=True,
+                        borders_innerV=True, borders_outerV=True, resizable=True,
+                        scrollX=True, policy=dpg.mvTable_SizingFixedFit):
+                for field in db.fields:
+                    dpg.add_table_column(label=field.replace('_', ' ').title())
+                data = db.get_municipio('CAMARGO')
+                for row in range(len(data)):
+                    for col in range(db.field_count):
+                        dpg.add_text(f'{data[row][col]}')
+                        if not (row == len(data) - 1 and col == db.field_count - 1):
+                            dpg.add_table_next_column()
+
+        with dpg.tab(label='5: Tempo de Espera na Fila'):
+            with dpg.table(header_row=True, row_background=True,
+                        borders_innerH=True, borders_outerH=True,
+                        borders_innerV=True, borders_outerV=True, resizable=True,
+                        scrollX=True, policy=dpg.mvTable_SizingFixedFit):
+                for field in db.fields:
+                    dpg.add_table_column(label=field.replace('_', ' ').title())
+                data = db.get_municipio('SAPIRANGA')
+                for row in range(len(data)):
+                    for col in range(db.field_count):
+                        dpg.add_text(f'{data[row][col]}')
+                        if not (row == len(data) - 1 and col == db.field_count - 1):
+                            dpg.add_table_next_column()
 
 dpg.set_primary_window(window, True)
 dpg.setup_dearpygui(viewport=vp)
 dpg.show_viewport(vp)
 
-dpg.show_item_registry()
-
 dpg.start_dearpygui()
 
-db.start()
-print(len(db.get()))
 db.close()
